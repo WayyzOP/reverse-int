5c602c26dc88d4a9bf66ce09ad62c85483e339b6
DMITRY MEDVEDEV <medvedevdmitry98@mail.ru>
2022-01-10 14:14:27 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let strNum = String(n);
+  let strNumArr = strNum.split('');
+
+  if (strNumArr[0] == '-') {
+      strNumArr.shift();
+  }
+
+  let reversedStrNumber = strNumArr.reverse().join('');
+
+  return reversedStrNumber;
 }
